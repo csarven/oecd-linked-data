c05f58e4c069c25901dfa8c63dc7fff300faacf8
Sarven Capadisli <info@csarven.ca>
2014-04-25 12:58:37 +0200

    Added remaining Interlinks

--- scripts/oecd.tdbloader.sh
+++ scripts/oecd.tdbloader.sh
@@ -30,36 +30,23 @@ for i in "$data"import/*.Structure.nt ; do java "$JVM_ARGS" tdb.tdbloader --desc
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data""$agency".prov.archive.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data""$agency".prov.retrieval.rdf
 
-
-#tail -n 266259 oecd.cl_location.nt > o.nt && sort -u o.nt > /data/oecd-linked-data/data/oecd.cl_location.nt
-#rapper -g oecd-worldbank.country.trigrams.accept.nt > /data/oecd-linked-data/data/oecd.exactMatch.worldbank.nt
-#rapper -g oecd-worldbank.country.trigrams.review.nt >> /data/oecd-linked-data/data/oecd.exactMatch.worldbank.nt
-#rapper -g oecd-transparency.country.trigrams.accept.nt > /data/oecd-linked-data/data/oecd.exactMatch.transparency.nt
-#rapper -g oecd-transparency.country.trigrams.review.nt >> /data/oecd-linked-data/data/oecd.exactMatch.transparency.nt
-#sed -i 's/exactMatch/closeMatch/' oecd-dbpedia.country.trigrams.review.nt
-#rapper -g oecd-dbpedia.country.trigrams.accept.nt > /data/oecd-linked-data/data/oecd.exactMatch.dbpedia.nt
-#rapper -g oecd-dbpedia.country.trigrams.review.nt >> /data/oecd-linked-data/data/oecd.exactMatch.dbpedia.nt
-#rapper -g oecd-bfs-location.trigrams.accept.nt > /data/oecd-linked-data/data/oecd.exactMatch.bfs.nt
-#rapper -g oecd-bfs-location.trigrams.review.nt >> /data/oecd-linked-data/data/oecd.exactMatch.bfs.nt
-#sort -u /data/oecd-linked-data/data/oecd.exactMatch.worldbank.nt > o.nt && mv o.nt /data/oecd-linked-data/data/oecd.exactMatch.worldbank.nt
-#sort -u /data/oecd-linked-data/data/oecd.exactMatch.transparency.nt > o.nt && mv o.nt /data/oecd-linked-data/data/oecd.exactMatch.transparency.nt
-#sort -u /data/oecd-linked-data/data/oecd.exactMatch.dbpedia.nt > o.nt && mv o.nt /data/oecd-linked-data/data/oecd.exactMatch.dbpedia.nt
-#sort -u /data/oecd-linked-data/data/oecd.exactMatch.bfs.nt > o.nt && mv o.nt /data/oecd-linked-data/data/oecd.exactMatch.bfs.nt
-
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.oecd.cl_location.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.worldbank.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.worldbank.currency.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.transparency.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.dbpedia.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.bfs.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.fao.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.ecb.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.imf.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.uis.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.frb.currency.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.eurostat.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.geonames.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.hr.nt
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.exactMatch.qudt.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.property.meta.nt
 java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$data"oecd.dataset.names.nt
-
 
 #real    146m42.351s
 #user    137m42.912s
